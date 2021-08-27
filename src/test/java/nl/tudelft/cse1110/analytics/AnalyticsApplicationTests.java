c5c2735e620ac3ce3fbcb5916427cb03b208a8d8
Yoon Hwan Jeong <yoonhwanjeong@gmail.com>
2021-08-28 00:51:46 +0200

    Initialize Spring Boot

---
+++ src/test/java/nl/tudelft/cse1110/analytics/AnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.tudelft.cse1110.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
