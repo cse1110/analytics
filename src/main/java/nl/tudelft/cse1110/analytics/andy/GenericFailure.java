c553224acffd01a14d6edda980fc1d5b1ab39bf7
Yoon Hwan Jeong <yoonhwanjeong@gmail.com>
2021-11-07 23:26:16 +0100

    Add GenericFailure

---
+++ src/main/java/nl/tudelft/cse1110/analytics/andy/GenericFailure.java
@@ -0,0 +1,4 @@
+package nl.tudelft.cse1110.analytics.andy;
+
+public record GenericFailure(String genericFailureMessage, String stepName, String exceptionMessage, Integer externalProcessExitCode, String externalProcessErrorMessages) {
+}
