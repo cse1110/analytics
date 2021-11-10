317e8a5744a637bd523b5c62cd8812a71ac46037
Yoon Hwan Jeong <yoonhwanjeong@gmail.com>
2021-11-11 00:47:26 +0100

    Add success message

--- src/main/java/nl/tudelft/cse1110/analytics/andy/Result.java
+++ src/main/java/nl/tudelft/cse1110/analytics/andy/Result.java
@@ -1,4 +1,4 @@
 package nl.tudelft.cse1110.analytics.andy;
 
-public record Result(CompilationResult compilation, UnitTestsResult tests, MutationTestingResult mutationTesting, CodeChecksResult codeChecks, CoverageResult coverage, MetaTestsResult metaTests, int finalGrade, GenericFailure genericFailure, double timeInSeconds, GradeWeight weights) {
+public record Result(CompilationResult compilation, UnitTestsResult tests, MutationTestingResult mutationTesting, CodeChecksResult codeChecks, CoverageResult coverage, MetaTestsResult metaTests, int finalGrade, GenericFailure genericFailure, double timeInSeconds, GradeWeight weights, String successMessage) {
 }
